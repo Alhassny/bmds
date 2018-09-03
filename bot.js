9cf54e7c6d16bbaac3664d9a45a201028323a66c
Alhassny <42678085+Alhassny@users.noreply.github.com>
2018-09-03 09:41:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -600,9 +600,46 @@ function play(guild, song) {
 
 });
 
+//join&leave
 
 
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
 
+  if (message.content === prefix + 'join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have **successfully** connected to the **channel**!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You **need **to join a **voice channel** first!');
+    }
+  }
+});
+
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === prefix + 'leave') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.leave()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have **successfully** leave to the **channel**!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You **need **to join a **voice channel** first!');
+    }
+  }
+});
 
 
 //admin
